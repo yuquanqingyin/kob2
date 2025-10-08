1a4a2d7aebd54fbbe6e5a6132baf531ade44c2c5
yuquanqingyin <1425821304@qq.com>
2025-10-08 16:25:53 +0800

    配置git环境与项目创建

---
+++ backend/src/main/java/com/kob/backend/Controller/pk/BotInfoController.java
@@ -0,0 +1,20 @@
+package com.kob.backend.Controller.pk;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/pk/")
+public class BotInfoController {
+
+    @RequestMapping("getbotinfo/")
+    public Map<String, String> getBotInfo() {
+        Map<String, String> bot = new HashMap<String, String>();
+        bot.put("name", "bot1");
+        bot.put("rating", "1500");
+        return bot;
+    }
+}
---
+++ web/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+<div>bot: {{ bot_name }}</div>
+<div>rating: {{ rating }}</div>
+<router-view></router-view>
+</template>
+
+<script>
+
+import {ref} from 'vue'
+import $ from 'jquery'
+
+export default {
+  name: 'App',
+  setup() {
+    let bot_name = ref("")
+    let rating = ref("")
+
+    $.ajax({
+      url: "http://localhost:8080/pk/getbotinfo/",
+      type: "GET",
+      success(resp) {
+        bot_name.value = resp.name
+        rating.value = resp.rating
+      },
+    });
+
+
+    return {
+      bot_name,
+      rating
+    }
+  }
+}
+
+</script>
+
+<style>
+
+</style>
