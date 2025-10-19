defb2dcc87ee4f04fd7fb62222da20a104d3f121
yuquanqingyin <1425821304@qq.com>
2025-10-19 10:53:09 +0800

    创建个人中心页面（上）Bot的后端增删改查

--- backend/src/main/java/com/kob/backend/controller/user/account/InfoController.java
+++ backend/src/main/java/com/kob/backend/controller/user/account/InfoController.java
@@ -1,11 +1,10 @@
-package com.kob.backend.controller.user;
+package com.kob.backend.controller.user.account;
 
 import com.kob.backend.service.user.account.InfoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
 import java.util.Map;
 
 @RestController
--- backend/src/main/java/com/kob/backend/controller/user/account/LoginController.java
+++ backend/src/main/java/com/kob/backend/controller/user/account/LoginController.java
@@ -1,9 +1,8 @@
-package com.kob.backend.controller.user;
+package com.kob.backend.controller.user.account;
 
 import com.kob.backend.service.user.account.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
