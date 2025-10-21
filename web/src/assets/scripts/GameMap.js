80a8912a869bef8787345e776baf9638815bf8a4
yuquanqingyin <1425821304@qq.com>
2025-10-21 19:57:40 +0800

    实现微服务：匹配系统（上）实现Websocket链接和云端同步生成地图

--- web/src/assets/scripts/GameMap.js
+++ web/src/assets/scripts/GameMap.js
@@ -3,11 +3,12 @@ import { Snake } from "./Snake";
 import { Wall } from './Wall'
 
 export class GameMap extends AcGameObject{
-    constructor(ctx, parent) {
+    constructor(ctx, parent, store) {
         super();
 
         this.ctx = ctx;
         this.parent = parent;
+        this.store = store;
         this.L = 0;
 
         this.cols = 13;
@@ -22,53 +23,56 @@ export class GameMap extends AcGameObject{
         ]
     }
 
-    check_connectivity(g, sx, sy, tx, ty) {
-        if(sx == tx && sy == ty) return true;
-        g[sx][sy] = true;
+    // check_connectivity(g, sx, sy, tx, ty) {
+    //     if(sx == tx && sy == ty) return true;
+    //     g[sx][sy] = true;
 
-        let dx = [-1, 0, 1, 0], dy = [0, 1, 0, -1];
-        for(let i = 0; i < 4; i++){
-            let x = sx + dx[i], y = sy + dy[i];
-            if(!g[x][y] && this.check_connectivity(g, x, y, tx, ty))
-                return true;
-        }
+    //     let dx = [-1, 0, 1, 0], dy = [0, 1, 0, -1];
+    //     for(let i = 0; i < 4; i++){
+    //         let x = sx + dx[i], y = sy + dy[i];
+    //         if(!g[x][y] && this.check_connectivity(g, x, y, tx, ty))
+    //             return true;
+    //     }
 
-        return false;
-    }
+    //     return false;
+    // }
 
     create_walls(){
-        const g = [];
-        for(let r = 0; r < this.rows; r++){
-            g[r] = [];
-            for(let c = 0; c < this.cols; c++){
-                g[r][c] = false;
-            }
-        }
+        // const g = [];
+        // for(let r = 0; r < this.rows; r++){
+        //     g[r] = [];
+        //     for(let c = 0; c < this.cols; c++){
+        //         g[r][c] = false;
+        //     }
+        // }
 
-        //给四周加上障碍物
-        for(let r = 0; r < this.rows; r++){
-            g[r][0] = g[r][this.cols - 1] = true;
-        }
-        for(let c = 0; c < this.cols; c++){
-            g[0][c] = g[this.rows - 1][c] = true;
-        }
+        // //给四周加上障碍物
+        // for(let r = 0; r < this.rows; r++){
+        //     g[r][0] = g[r][this.cols - 1] = true;
+        // }
+        // for(let c = 0; c < this.cols; c++){
+        //     g[0][c] = g[this.rows - 1][c] = true;
+        // }
 
-        //创建随机障碍物
-        for(let i = 0; i < this.inner_walls_counts / 2; i++){
-            for(let j = 0; j < 1000; j++){
-                let r = parseInt(Math.random() * this.rows);
-                let c = parseInt(Math.random() * this.cols);
-                if(g[r][c] || g[this.rows - 1 - r][this.cols - 1 - c]) continue;
-                if(r == this.rows - 2 && c == 1 || r == 1 && c == this.cols -2) 
-                    continue;
+        // //创建随机障碍物
+        // for(let i = 0; i < this.inner_walls_counts / 2; i++){
+        //     for(let j = 0; j < 1000; j++){
+        //         let r = parseInt(Math.random() * this.rows);
+        //         let c = parseInt(Math.random() * this.cols);
+        //         if(g[r][c] || g[this.rows - 1 - r][this.cols - 1 - c]) continue;
+        //         if(r == this.rows - 2 && c == 1 || r == 1 && c == this.cols -2) 
+        //             continue;
 
-                g[r][c] = g[this.rows - 1 - r][this.cols - 1 - c] = true;
-                break;
-            }
-        }
+        //         g[r][c] = g[this.rows - 1 - r][this.cols - 1 - c] = true;
+        //         break;
+        //     }
+        // }
 
-        const copy_g = JSON.parse(JSON.stringify(g));
-        if(!this.check_connectivity(copy_g, this.rows - 2, 1, 1, this.cols - 2)) return false;
+        // const copy_g = JSON.parse(JSON.stringify(g));
+        // if(!this.check_connectivity(copy_g, this.rows - 2, 1, 1, this.cols - 2)) return false;
+
+        const g = this.store.state.pk.gamemap;
+        console.log(g);
 
         for(let r = 0; r < this.rows; r++){
             for(let c = 0; c < this.cols; c++) {
@@ -78,7 +82,7 @@ export class GameMap extends AcGameObject{
             }
         }
 
-        return true;
+        // return true;
     }
 
     add_listening_events(){
@@ -98,9 +102,10 @@ export class GameMap extends AcGameObject{
     }
 
     start() {
-        for(let i = 0; i < 1000; i++)
-            if(this.create_walls())
-                break;
+        this.create_walls();
+        // for(let i = 0; i < 1000; i++)
+        //     if(this.create_walls())
+        //         break;
         this.add_listening_events();
     }
 
--- web/src/views/pk/PkIndexView.vue
+++ web/src/views/pk/PkIndexView.vue
@@ -1,18 +1,71 @@
 <template>
 
-    <PlayGround/>
+    <PlayGround v-if="$store.state.pk.status === 'playing'"/>
+
+    <MatchGround v-if="$store.state.pk.status === 'matching'"/>
 
 </template>
 
 
 <script>
 import PlayGround from '@/components/PlayGround.vue';
+import { onMounted, onUnmounted } from 'vue';
+// import { useStore } from 'vuex';
+import MatchGround from '@/components/MatchGround.vue';
+import store from '@/store';
 
 export default{
 
     components: {
         PlayGround,
-    }
+        MatchGround
+    },
+    setup() {
+
+        // const store = useStore();
+        const socketUrl = `ws://localhost:8080/websocket/${localStorage.getItem('jwt_token')}/`;
+        let socket = null;
+
+
+        onMounted(() => {
+
+            store.commit("updateOpponent", {
+                username: "我的对手",
+                photo: "https://cdn.acwing.com/media/article/image/2022/08/09/1_1db2488f17-anonymous.png"
+            })
+
+            socket = new WebSocket(socketUrl);
+
+            socket.onopen = () => {
+                console.log('WebSocket connection established.');
+                store.commit("updateSocket", socket);
+            };
+
+            socket.onmessage = msg => {
+                const data = JSON.parse(msg.data);
+                if(data.event === "start-matching"){
+                    store.commit("updateOpponent", {
+                        username: data.opponent_username,
+                        photo: data.opponent_photo
+                    });
+                    setTimeout(() => {
+                        store.commit("updateStatus", "playing");
+                    }, 2000);
+                    store.commit("updateGamemap", data.gamemap);
+                }
+            };
+
+            socket.onclose = () => {
+                console.log('WebSocket connection closed.');
+            };
+        });
+
+        onUnmounted(() => {
+            socket.close();
+            store.commit("updateStatus", "matching");
+        });
+
+    },
 }
 
 
